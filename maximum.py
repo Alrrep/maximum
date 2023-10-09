d693add66214f53130e629af4bb3210133ab6036
Alrrep <perlaa.rmn@gmail.com>
2023-10-09 12:56:54 -0600

    deleting hardcore

--- maximum.py
+++ maximum.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 #Maximum algorithm
-
+import sys
 data=[1.0, 3.14, 6.2, 0.1, 5.3]
 
-maximum= -999999.99
+maximum=- sys.float_info.max
 for x in data:
     if x >maximum:
         maximum=x
