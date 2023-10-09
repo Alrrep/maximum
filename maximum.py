7d74df8a9e68d93ac3e77bf8810fc591d259a8b3
Alrrep <perlaa.rmn@gmail.com>
2023-10-09 12:41:11 -0600

    maxinum

---
+++ maximum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Maximum algorithm
+
+data=[1.0, 3.14, 6.2, 0.1, 5.3]
+
+maximum= -999999.99
+for x in data:
+    if x >maximum:
+        maximum=x
+    #print(x)
+print(maximum)
